62b733602baa0f2a6ba90d7a49ccb141fdbbf314
maxdavid <me@maxdavid.com>
2019-12-15 20:22:11 -0800

    Set up initial hook/context/reducer model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import * as reset from './styles/reset.css';
+import * as global from './styles/global.css';
+
+import { StateProvider } from './contexts/StateProvider';
+import { initialState } from './reducers/initialState';
+import { rootReducer } from './reducers/rootReducer';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    ${reset} 
+    ${global}
+`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const theme = {
+  grayAccent: '#f3f3f3',
+  yellowAccent: '#eeff41',
+  darkGray: '#666',
+  tinyFont: '1.2rem',
+  smallFont: '1.5rem',
+  mediumFont: '1.8rem',
+  largeFont: '2.2rem',
+  hugeFont: '3.0rem',
+  headerSize: '6.0rem'
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Router>
+      <StateProvider initialState={initialState} reducer={rootReducer}>
+        <App />
+      </StateProvider>
+    </Router>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
