46409fb52bd9ae4eb230186b15144daaab76ff26
Max David <me@maxdavid.com>
2020-01-02 16:36:26 -0800

    Make picture circular, add social links

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { device } from '../../styles/deviceBreakpoints';
 
 import FunShapes from './FunShapes';
-import { maxHeadshot } from './assets';
+import { maxHeadshot, maxHeadshotSquare } from './assets';
 
 export const Bio = props => {
   return (
@@ -17,14 +17,102 @@ export const Bio = props => {
         <BioContact className='mono'>
           <a href='mailto:me@maxdavid.com'>get in contact</a>
         </BioContact>
+        <BioLinksContainer>
+          <BioLink
+            href='https://github.com/maxdavid'
+            rel='noopener noreferred'
+            target='_blank'
+            title='GitHub'
+          >
+            <div className='container'>
+              <h6>Some of my Code</h6>
+              <h4>GitHub</h4>
+            </div>
+          </BioLink>
+          <BioLink
+            href='https://www.linkedin.com/in/maxdavid/'
+            rel='noopener noreferred'
+            target='_blank'
+            title='LinkedIn'
+          >
+            <div className='container'>
+              <h6>Connect if you Want</h6>
+              <h4>LinkedIn</h4>
+            </div>
+          </BioLink>
+        </BioLinksContainer>
       </BioText>
       <BioImage>
-        <img src={maxHeadshot} alt='headshot of Max David' />
+        <img src={maxHeadshotSquare} alt='headshot of Max David' />
       </BioImage>
       <FunShapes />
     </StyledBio>
   );
 };
+
+const BioLinksContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  padding-top: 3em;
+  & > * + * {
+    margin-left: 1em;
+  }
+`;
+
+const BioLink = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  display: block;
+  background-color: ${({ theme }) => theme.surface};
+  border: 1px solid ${({ theme }) => theme.grayAccent};
+  border-radius: 5px;
+  transition: all 0.25s ease;
+  position: relative;
+
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  .container {
+    padding: 10px;
+  }
+
+  h6 {
+    line-height: 1;
+    font-weight: 400;
+    margin: 0;
+  }
+
+  h4 {
+    line-height: 1.2;
+    font-weight: 800;
+    margin: 0;
+  }
+
+  &:hover {
+    transition: all 0.25s ease;
+    transform: scale(1.05);
+    background-color: ${({ theme }) => theme.yellowAccent};
+  }
+
+  &::after {
+    display: block;
+    font-weight: 900;
+    height: 100%;
+    content: '>';
+    font-size: 40px;
+    padding: 0 10px;
+    border-right: 5px solid ${({ theme }) => theme.yellowAccent};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+  }
+`;
 
 const StyledBio = styled.div`
   width: 100%;
@@ -52,6 +140,7 @@ const BioImage = styled.div`
 
   img {
     object-fit: contain;
+    border-radius: 50%;
   }
 
   @media ${device.tablet} {
