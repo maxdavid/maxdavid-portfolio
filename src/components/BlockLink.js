cb5c9724c091bab7f61a0677370bc91dd720e672
Max David <me@maxdavid.com>
2020-01-13 17:23:38 -0800

    BioLinks -> BlockLinks

---
+++ src/components/BlockLink.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const BlockLink = props => {
+  return (
+    <StyledBlockLink
+      href={props.href}
+      title={props.title}
+      rel='noopener noreferred'
+      target='_blank'
+    >
+      <div className='container'>
+        <h6>{props.description}</h6>
+        <h4>{props.title}</h4>
+      </div>
+    </StyledBlockLink>
+  );
+};
+
+export const StyledBlockLinksContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  padding-top: 3em;
+  & > * + * {
+    margin-left: 1em;
+  }
+`;
+
+export const StyledBlockLink = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  display: block;
+  background-color: ${({ theme }) => theme.surface};
+  border: 1px solid ${({ theme }) => theme.grayAccent};
+  border-radius: 5px;
+  transition: all 0.25s ease;
+  position: relative;
+
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  .container {
+    padding: 10px;
+  }
+
+  h6 {
+    line-height: 1;
+    font-weight: 400;
+    margin: 0;
+  }
+
+  h4 {
+    line-height: 1.2;
+    font-weight: 800;
+    margin: 0;
+  }
+
+  &:hover {
+    transition: all 0.25s ease;
+    transform: scale(1.05);
+    background-color: ${({ theme }) => theme.yellowAccent};
+  }
+
+  &::after {
+    display: block;
+    font-weight: 900;
+    height: 100%;
+    content: '>';
+    font-size: 40px;
+    padding: 10px 10px 0 10px;
+    border-right: 5px solid ${({ theme }) => theme.yellowAccent};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+  }
+`;
--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { device } from '../../styles/deviceBreakpoints';
 
 import FunShapes from './FunShapes';
+import { BlockLink, StyledBlockLinksContainer } from '../BlockLink';
 import { maxHeadshot, maxHeadshotSquare } from './assets';
 
 export const Bio = props => {
@@ -18,30 +19,18 @@ export const Bio = props => {
         <BioContact className='mono'>
           <a href='mailto:me@maxdavid.com'>✎ get in contact</a>
         </BioContact>
-        <BioLinksContainer>
-          <BioLink
+        <StyledBlockLinksContainer>
+          <BlockLink
             href='https://github.com/maxdavid'
-            rel='noopener noreferred'
-            target='_blank'
             title='GitHub'
-          >
-            <div className='container'>
-              <h6>Some of my Code</h6>
-              <h4>GitHub</h4>
-            </div>
-          </BioLink>
-          <BioLink
+            description='Some of my Code'
+          />
+          <BlockLink
             href='https://www.linkedin.com/in/maxdavid/'
-            rel='noopener noreferred'
-            target='_blank'
             title='LinkedIn'
-          >
-            <div className='container'>
-              <h6>Connect if you Want</h6>
-              <h4>LinkedIn</h4>
-            </div>
-          </BioLink>
-        </BioLinksContainer>
+            description='Connect if you Want'
+          />
+        </StyledBlockLinksContainer>
       </BioText>
       <BioImage>
         <img src={maxHeadshotSquare} alt='headshot of Max David' />
@@ -50,70 +39,6 @@ export const Bio = props => {
     </StyledBio>
   );
 };
-
-const BioLinksContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  padding-top: 3em;
-  & > * + * {
-    margin-left: 1em;
-  }
-`;
-
-const BioLink = styled.a`
-  cursor: pointer;
-  text-decoration: none;
-  display: block;
-  background-color: ${({ theme }) => theme.surface};
-  border: 1px solid ${({ theme }) => theme.grayAccent};
-  border-radius: 5px;
-  transition: all 0.25s ease;
-  position: relative;
-
-  flex-grow: 1;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
-  .container {
-    padding: 10px;
-  }
-
-  h6 {
-    line-height: 1;
-    font-weight: 400;
-    margin: 0;
-  }
-
-  h4 {
-    line-height: 1.2;
-    font-weight: 800;
-    margin: 0;
-  }
-
-  &:hover {
-    transition: all 0.25s ease;
-    transform: scale(1.05);
-    background-color: ${({ theme }) => theme.yellowAccent};
-  }
-
-  &::after {
-    display: block;
-    font-weight: 900;
-    height: 100%;
-    content: '>';
-    font-size: 40px;
-    padding: 10px 10px 0 10px;
-    border-right: 5px solid ${({ theme }) => theme.yellowAccent};
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-  }
-`;
 
 const StyledBio = styled.div`
   width: 100%;
